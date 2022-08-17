bb97e851e6a52966c9682e312bb5708d76fdae76
DanielAvilez1 <104475313+DanielAvilez1@users.noreply.github.com>
2022-08-16 22:41:15 -0700

    fix

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*)$g@@f$#m#6i$j1b&icyhq^3qlzs#aavvmu+7+^qqj3l$b^pa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".heroku"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
